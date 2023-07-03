1dd4f444e6fe4be7ca1447ea4f63420946833b54
RinaJoseArulselvan <135804163+RinaJoseArulselvan@users.noreply.github.com>
2023-07-03 15:51:38 +0530

    Update ImageDownloadCoroutine.kt

--- CoroutineAssignment/ImageDownloadCoroutine.kt
+++ CoroutineAssignment/ImageDownloadCoroutine.kt
@@ -1,80 +1,46 @@
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+package Assignment
 import kotlinx.coroutines.*
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 import java.io.File
 import java.net.URL
+class ImageDownloadCoroutine {
+    fun downloadImage(url: String, outputDir: String) {
+        val fileName = url.substringAfterLast("/")
+        val outputFile = File(outputDir, fileName)
 
-
-suspend fun scrapeImagesFromUrl(url: String) {
-    val outputDir = "C:\\Users\\RINA JOSE\\Documents\\Images"
-    try {
-        val document: Document = Jsoup.connect(url).get()
-        val images: List<Element> = document.select("img")
-
-        for (image in images) {
-            val imageUrl = image.absUrl("src")
-            // You can download or process the image URL here
-            downloadImage(imageUrl, outputDir)
-        }
-
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
-}
-
-fun downloadImage(url: String, outputDir: String) {
-    val fileName = url.substringAfterLast("/")
-    val outputFile = File(outputDir, fileName)
-
-    try {
-        val connection = URL(url).openConnection()
-        connection.getInputStream().use { input ->
-            outputFile.outputStream().use { output ->
-                input.copyTo(output)
+        try {
+            val connection = URL(url).openConnection()
+            connection.getInputStream().use { input ->
+                outputFile.outputStream().use { output ->
+                    input.copyTo(output)
+                }
             }
-        }
-        println("Downloaded: $url")
+            println("Downloaded: $url")
 
-    } catch (e: Exception) {
-        println("Failed to download: $url")
+        } catch (e: Exception) {
+            println("Failed to download: $url")
+        }
     }
 }
-
 fun main() {
 
-    val urls = listOf(
-        "https://www.amazon.in/",
-        "https://www.kaggle.com/",
-        "https://www.udemy.com/",
-        "https://moz.com/",
-        "https://www.similarweb.com/",
-        "https://www.entrepreneur.com/",
-        "https://www.wix.com/",
-        "https://time.com/",
-        "https://www.computerhope.com/",
-        "https://squareboat.com/",
-        "https://www.wikipedia.org/",
-        "https://www.weebly.com/",
-        "https://www.google.com/",
-        "https://www.reddit.com/",
-        "https://www.twitter.com/",
-        "https://www.facebook.com/",
-        "https://www.jetbrains.com",
-        "https://stackoverflow.com/",
-        "https://www.eclipse.org/",
-        "https://wiki.gnome.org/"
+    val imageDownloadCoroutine = ImageDownloadCoroutine()
+    val directory = "C:\\Users\\rinajose.a\\Downloads\\images"
+    val imageUrls = listOf(
+        "https://img.freepik.com/premium-photo/colorful-flowers-colorful-background_900958-10009.jpg",
+        "https://img.freepik.com/premium-photo/colorful-flowers-white-background_900958-10024.jpg",
+        "https://img.freepik.com/premium-photo/colorful-floral-pattern-with-flowers-leaves_900958-4862.jpg",
+        // Add more image URLs here
     )
-    runBlocking {
-        val tasks = urls.map { url ->
-            async {
-                scrapeImagesFromUrl(url)
 
+  runBlocking {
+        val tasks = imageUrls.map { url ->
+            async {
+                imageDownloadCoroutine.downloadImage(url, directory)
             }
         }
-        tasks.awaitAll()
+        tasks.forEach {
+            it.join()
+        }
         println("All images downloaded successfully.")
     }
 }
